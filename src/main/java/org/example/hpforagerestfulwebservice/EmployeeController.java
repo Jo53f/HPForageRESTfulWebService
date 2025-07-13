f05960109c8e5001b565648f1b8642f567ba61b5
josef <josef.abdulkarim@hotmail.co.uk>
2025-07-13 22:54:09 +0100

    updated to best practice

--- src/main/java/org/example/hpforagerestfulwebservice/EmployeeController.java
+++ src/main/java/org/example/hpforagerestfulwebservice/EmployeeController.java
@@ -1,8 +1,11 @@
 package org.example.hpforagerestfulwebservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -16,9 +19,17 @@ class EmployeeController {
         return employeesM.getEmployees();
     }
 
-    @PostMapping("/add")
-    public void addEmployee(@RequestBody Employee employee) {
+    @PostMapping(value = "/add", consumes = "application/json")
+    public ResponseEntity<Object> addEmployee(@RequestBody Employee employee) {
         employeesM.addEmployee(employee);
+
+        URI employeeLocation = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(employee.employee_id())
+                .toUri();
+
+        return ResponseEntity.created(employeeLocation).build();
     }
 
 }
